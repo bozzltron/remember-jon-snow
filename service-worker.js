5b4172fac6fcad1acab6cda1a9db62a5bcd1a33a
Michael <michael.bosworth@bunchball.com>
2019-06-15 19:58:00 -0500

    remove precache

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute([
-  '/',
-  '/style.css',
-  '/jon512x512.png',
-  '/jon192x192.png',
-  '/audio/careful.mp3', 
-  '/audio/defend_self.mp3', 
-  '/audio/deliver_baby.mp3', 
-  '/audio/risk_lives.mp3'
-]);
+// workbox.precaching.precacheAndRoute([
+//   '/',
+//   '/style.css',
+//   '/jon512x512.png',
+//   '/jon192x192.png',
+//   '/audio/careful.mp3', 
+//   '/audio/defend_self.mp3', 
+//   '/audio/deliver_baby.mp3', 
+//   '/audio/risk_lives.mp3'
+// ]);
 
 workbox.routing.registerRoute(
   '/',
